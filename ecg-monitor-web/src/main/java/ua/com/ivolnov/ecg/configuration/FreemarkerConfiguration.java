a9ffd0434ad0ca6c29c49cd56a95483effa77ef1
ivolnov <holyloader@gmail.com>
2025-05-31 23:43:28 +0300

    chore [ECG-1]: create project backbone

---
+++ ecg-monitor-web/src/main/java/ua/com/ivolnov/ecg/configuration/FreemarkerConfiguration.java
@@ -0,0 +1,20 @@
+package ua.com.ivolnov.ecg.configuration;
+
+import freemarker.template.TemplateModelException;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+class FreemarkerConfiguration {
+
+    private final FreeMarkerConfigurer freeMarkerConfigurer;
+
+    @PostConstruct
+    public void configuration() throws TemplateModelException {
+        freeMarkerConfigurer.getConfiguration().setSharedVariable("dummy_var", "dummy_value");
+    }
+
+}
