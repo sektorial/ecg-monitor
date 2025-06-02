95cf4cd89e39e2c9a21f367e95059ee9fa4cd69d
ivolnov <holyloader@gmail.com>
2025-06-02 04:35:43 +0300

    feat [ECG-5]: display Patients on Staff landing page

--- ecg-monitor-web/src/main/java/ua/com/ivolnov/ecg/StaffLandingViewController.java
+++ ecg-monitor-web/src/main/java/ua/com/ivolnov/ecg/StaffLandingViewController.java
@@ -1,19 +1,25 @@
 package ua.com.ivolnov.ecg;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import ua.com.ivolnov.ecg.patient.PatientService;
 
-@RequestMapping("/web/staff")
 @Controller
+@RequestMapping("/web/staff")
+@RequiredArgsConstructor
 public class StaffLandingViewController {
+
+    private final PatientService patientService;
 
     @GetMapping
     public ModelAndView getHelloWorld() {
-        final ModelAndView modelAndView = new ModelAndView("staff-landing");
-        modelAndView.getModel().put("msg", "Hello Staff");
+        ModelAndView modelAndView = new ModelAndView("staff-landing");
+        modelAndView.addObject("msg", "Hello Staff");
+        modelAndView.addObject("patients", patientService.getAllPatients());
         return modelAndView;
     }
-
 }
+
