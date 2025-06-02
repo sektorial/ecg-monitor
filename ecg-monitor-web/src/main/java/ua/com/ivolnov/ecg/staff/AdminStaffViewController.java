9458d7c853ec24167b885915ae46fd7cca277b41
ivolnov <holyloader@gmail.com>
2025-06-02 04:06:43 +0300

    feat [ECG-2.4]: implement Staff management by Admin

--- ecg-monitor-web/src/main/java/ua/com/ivolnov/ecg/staff/AdminStaffViewController.java
+++ ecg-monitor-web/src/main/java/ua/com/ivolnov/ecg/staff/AdminStaffViewController.java
@@ -1,19 +1,41 @@
 package ua.com.ivolnov.ecg.staff;
 
+import java.util.Set;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/web/admin/staff")
 @Controller
+@RequiredArgsConstructor
 public class AdminStaffViewController {
 
+    private final MedicalStaffService staffService;
+
     @GetMapping
-    public ModelAndView getHelloWorld() {
-        final ModelAndView modelAndView = new ModelAndView("admin-staff");
-        modelAndView.getModel().put("msg", "Staff management will be here...");
-        return modelAndView;
+    public ModelAndView staffPage() {
+        final ModelAndView mav = new ModelAndView("admin-staff");
+        final Set<MedicalStaff> staff = staffService.getAllStaff();
+        mav.getModel().put("msg", "Staff Management");
+        mav.getModel().put("staff", staff);
+        return mav;
+    }
+
+    @PostMapping("/add")
+    public String addStaff(@RequestParam final String username, @RequestParam final String password) {
+        staffService.addStaff(username, password);
+        return "redirect:/web/admin/staff";
+    }
+
+    @PostMapping("/remove")
+    public String removeStaff(@RequestParam final String username) {
+        staffService.removeStaff(username);
+        return "redirect:/web/admin/staff";
     }
 
 }
