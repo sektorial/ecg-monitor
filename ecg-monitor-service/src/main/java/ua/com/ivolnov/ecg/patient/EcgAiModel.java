db6ea9909509a217663e6752f7523099da752ffa
ivolnov <holyloader@gmail.com>
2025-06-03 18:08:26 +0300

    feat [ECG-10]: implement the simplest stub of an AI Model to analyze ECG data by an individual sample

---
+++ ecg-monitor-service/src/main/java/ua/com/ivolnov/ecg/patient/EcgAiModel.java
@@ -0,0 +1,18 @@
+package ua.com.ivolnov.ecg.patient;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Current version is the simplest stub for POC. In real-world scenario AI Model should  the shape and evolution of
+ * the waveform over time, not by an individual sample
+ */
+@Component
+class EcgAiModel {
+
+    private static final double SPIKE_THRESHOLD = 1.5;
+
+    boolean isCriticalSpike(double voltage) {
+        return Math.abs(voltage) > SPIKE_THRESHOLD;
+    }
+
+}
